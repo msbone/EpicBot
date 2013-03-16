dbefedc149c3d7c156b84f6323ad48650d202f8a
Ole Mathias Aa. Heggem <msbone1995@gmail.com>
2013-03-16 01:34:14 +0100

    Added some fixes to the laser

--- src/net/msbone/epicbot/Weapon.java
+++ src/net/msbone/epicbot/Weapon.java
@@ -11,51 +11,124 @@ public class Weapon {
 	public static boolean InRange(int posJ, int posK, int enemyJ, int enemyK, String weapon, Object[][] kart){
 		//Do a check if player standing at posK/posJ can attack using weapon
 		
-		int laserRange = 4;
-		
-		//Top-right = J - 1
-		//Bot-right = K + 1
-		//Top-left = K - 1
-		//Bot-left = J + 1
+		int laserRange = 5;
 		
 		if(weapon.equalsIgnoreCase("laser") && 1==1) {
-			int topJ = posJ - laserRange;
-			int topK = posK - laserRange;
 			
-			int botJ = posJ + laserRange;
-			int botK = posK + laserRange;
-						
-			if(enemyJ < posJ && enemyK < posK && enemyJ > topJ && enemyK > topK) {
-				//The point is over the player
-				System.out.println("I have a player in sight, LETS SHOT");
-				//FIRE
-				Gson gson = new Gson();
-				Map<String, String> moveMessage = new HashMap<String, String>();
-				moveMessage.put("message", "action");
-				moveMessage.put("type", "laser");
-				moveMessage.put("direction", "up");
-				String json = gson.toJson(moveMessage);
-				Client.sendMessage(json);
-				
-				return true;
-			}
+			int topJfinal;
+			int topKfinal;
 			
-			if(enemyJ > posJ && enemyK > posK && enemyJ < botJ && enemyK < botK) {
-				//The point is under the player
-				System.out.println("I have a player in sight");
-				Gson gson = new Gson();
-				Map<String, String> moveMessage = new HashMap<String, String>();
-				moveMessage.put("message", "action");
-				moveMessage.put("type", "laser");
-				moveMessage.put("direction", "down");
-				String json = gson.toJson(moveMessage);
-				Client.sendMessage(json);
-				return true;
-			}
-			else {
-				System.out.println("No player in sight");
-				System.out.println();
+			int botJfinal;
+			int botKfinal;
+			
+			int lefttopJfinal;
+			int lefttopKfinal;
+			
+			int leftbotJfinal;
+			int leftbotKfinal;
+			
+			int righttopJfinal;
+			int righttopKfinal;
+			
+			int rightbotJfinal;
+			int rightbotKfinal;
+			
+			
+			int x = 0;
+			while(x < laserRange) {
 				
+				//Move to all the sides
+				topJfinal = posJ + x;
+				topKfinal = posK + x;
+				
+				botJfinal = posJ - x;
+				botKfinal = posK - x;
+				
+				lefttopJfinal = posJ;
+				lefttopKfinal = posK - x;
+				
+				leftbotJfinal = posJ + x;
+				leftbotKfinal = posK;
+				
+				righttopJfinal = posJ - x;
+				righttopKfinal = posK;
+				
+				rightbotJfinal = posJ + x;
+				rightbotKfinal = posK;
+				
+				if(topJfinal == enemyJ && topKfinal == enemyK) {
+					//Shoot down
+					Gson gson = new Gson();
+					Map<String, String> moveMessage = new HashMap<String, String>();
+					moveMessage.put("message", "action");
+					moveMessage.put("type", "laser");
+					moveMessage.put("direction", "down");
+					String json = gson.toJson(moveMessage);
+					Client.sendMessage(json);
+					return true;
+				}
+				
+				if(botJfinal == enemyJ && botKfinal == enemyK) {
+					//Shoot up
+					Gson gson = new Gson();
+					Map<String, String> moveMessage = new HashMap<String, String>();
+					moveMessage.put("message", "action");
+					moveMessage.put("type", "laser");
+					moveMessage.put("direction", "up");
+					String json = gson.toJson(moveMessage);
+					Client.sendMessage(json);
+					return true;
+				}
+				
+				if(lefttopJfinal == enemyJ && lefttopKfinal == enemyK) {
+					//Shoot left top
+					Gson gson = new Gson();
+					Map<String, String> moveMessage = new HashMap<String, String>();
+					moveMessage.put("message", "action");
+					moveMessage.put("type", "laser");
+					moveMessage.put("direction", "left-up");
+					String json = gson.toJson(moveMessage);
+					Client.sendMessage(json);
+					return true;
+				}
+				
+				if(leftbotJfinal == enemyJ && leftbotKfinal == enemyK) {
+					//Shoot left down
+					Gson gson = new Gson();
+					Map<String, String> moveMessage = new HashMap<String, String>();
+					moveMessage.put("message", "action");
+					moveMessage.put("type", "laser");
+					moveMessage.put("direction", "left-down");
+					String json = gson.toJson(moveMessage);
+					Client.sendMessage(json);
+					return true;
+				}
+				
+				if(righttopJfinal == enemyJ && righttopKfinal == enemyK) {
+					//Shoot right top
+					Gson gson = new Gson();
+					Map<String, String> moveMessage = new HashMap<String, String>();
+					moveMessage.put("message", "action");
+					moveMessage.put("type", "laser");
+					moveMessage.put("direction", "right-up");
+					String json = gson.toJson(moveMessage);
+					Client.sendMessage(json);
+					return true;
+				}
+				
+				if(rightbotJfinal == enemyJ && rightbotKfinal == enemyK) {
+					//Shoot right down
+					Gson gson = new Gson();
+					Map<String, String> moveMessage = new HashMap<String, String>();
+					moveMessage.put("message", "action");
+					moveMessage.put("type", "laser");
+					moveMessage.put("direction", "right-down");
+					String json = gson.toJson(moveMessage);
+					Client.sendMessage(json);
+					return true;
+				}
+				
+				x++;
 			}
 			
 		}
